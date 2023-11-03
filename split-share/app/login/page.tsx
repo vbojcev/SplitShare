14d61e87e6e5fad9b4d1a75258590ce43e87e5d0
vbojcev <vbojcev@uwaterloo.ca>
2023-11-03 15:05:10 -0400

    start login page

--- split-share/app/login/page.tsx
+++ split-share/app/login/page.tsx
@@ -1,4 +1,23 @@
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 const page = () => {
+
+  //pull user session:
+  const { data: session } = useSession();
+
+  const router = useRouter()
+
+  useEffect(() => {
+
+    // If user is logged in, shouldn't be able to view login page.
+    if (session?.user) {
+      router.push('/');
+    }
+
+  }, []);
+
   return (
     <div className="relative flex place-items-center">
       <p>Login Placeholder</p>
